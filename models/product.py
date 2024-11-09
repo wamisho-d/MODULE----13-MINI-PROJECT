22373760c9e0d88aab9b3cb8af5505822df792a3
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2024-11-09 15:18:01 -0500

    Update product.py

---
+++ models/product.py
@@ -0,0 +1,8 @@
+from . import db
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    order_items = db.relationship('OrderItem', back_populates='product')
